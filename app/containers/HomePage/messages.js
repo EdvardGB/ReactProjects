e673a54c40c15d567b957eb5dd5118b1a4e6d02b
EdvardGB <edvard_bakken@live.no>
2018-04-19 18:06:11 +0200

    first commit

---
+++ app/containers/HomePage/messages.js
@@ -0,0 +1,17 @@
+/*
+ * HomePage Messages
+ *
+ * This contains all the text for the HomePage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.HomePage.header',
+    defaultMessage: 'This is HomePage component!',
+  },
+  humidity:{
+  	id:'app.components.HomePage.humidity',
+  	defaultMessage: 'Humidity = ',
+  }
+});
